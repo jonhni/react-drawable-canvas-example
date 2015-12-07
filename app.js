85f04d5af1c055b8cef93fdaf0bddba973fc6d75
Jon Nilsson <jon.h.nilsson@gmail.com>
2015-12-07 11:41:11 +0100

    use port variable

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(serveStatic(path.join(__dirname, 'public')));
-app.listen(3000, () =>{
+app.listen(port, () =>{
   console.log("Server running on port: ", port);
 });
